235e1167ef7f48afa944097d22e518376ce713b1
Ben Teichman <ben.l.teichman@gmail.com>
2017-02-11 15:54:35 -0500

    fix(publish): pipe promise to publish correctly

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import execa from 'execa';
 import preRelease from './pre';
 import postRelease from './post';
 
-const publish = (log) => execa('npm', ['publish'])
-  .then((res) => log(res.stdout));
+const publish = (log) => {
+  log('publishing to npm');
+
+  return execa.shell('npm', ['publish'])
+    .then((res) => {
+      if (res.stderr) {
+        throw new Error(res.stderr);
+      }
+
+      return log(res.stdout);
+    });
+};
 
 export default (opts) => () => {
   return function release(log) {
     return preRelease(opts, log)
-      .then(() => publish())
+      .then(() => publish(log))
       .then(() => postRelease(opts, log));
   };
 };
