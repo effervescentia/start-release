4939a670215d95ce83b8250d4723cbcdd4bc5cf8
Ben Teichman <ben.l.teichman@gmail.com>
2017-02-11 14:41:15 -0500

    switch spork for execa

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
+import execa from 'execa';
 import preRelease from './pre';
 import postRelease from './post';
 
-const publish = () => new Promise((resolve, reject) => {
-  require('spork')('npm', ['publish'], { exit: false, quiet: true })
-    // eslint-disable-next-line no-confusing-arrow, no-ternary, no-magic-numbers
-    .on('exit:code', (code) => code === 0 ?
-      resolve() :
-      reject(new Error('failed to publish to npm')));
-});
+const publish = (log) => execa('npm', ['publish'])
+  .then((res) => log(res.stdout));
 
 export default (opts) => () => {
   return function release(log) {
